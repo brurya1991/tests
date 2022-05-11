48285bfc4597a1dbbf9c6641e7c7ac629f1d0d4a
brurya1991 <31319140+brurya1991@users.noreply.github.com>
2022-05-11 19:27:03 +0300

    Add files via upload

---
+++ echo_test_suite.py
@@ -0,0 +1,52 @@
+import http.client
+import pytest
+
+from pythonProject.venv import my_server
+
+
+@pytest.fixture()
+def http_server():
+    server = my_server.ServerHttpUtils()
+    server.create_server()
+    yield server
+    server.close_server()
+
+
+@pytest.fixture()
+def http_client(http_server):
+    connection = http.client.HTTPConnection(http_server.SERVER_HOST, http_server.SERVER_PORT, timeout=10)
+    yield connection
+    connection.close()
+
+
+def send_request(http_client):
+    try:
+        http_client.request("GET", "/")
+        response = http_client.getresponse()
+    except ConnectionRefusedError:
+        return False
+    return response
+
+
+def validate_expected_response(response):
+    if response:
+        print("Status: {} and reason: {}".format(response.status, response.reason))
+        if response.status != 200 or response.reason != "OK":
+            pytest.fail(f"response result is:\nStatus: {response.status} and Reason: {response.reason} "
+                        f"\nnot as expected: Status: 200 and reason: OK")
+    else:
+        pytest.fail("There is NO response!")
+
+
+def validate_no_response(response):
+    if response:
+        print("Status: {} and reason: {}".format(response.status, response.reason))
+        pytest.fail("Got a response! Not as expected!")
+    else:
+        print("There is NO response as expected")
+
+
+def test_send_request(http_server, http_client):
+    validate_expected_response(send_request(http_client))
+    http_server.close_server()
+    validate_no_response(send_request(http_client))
---
+++ my_server.py
@@ -0,0 +1,41 @@
+import socket
+from threading import Thread
+
+
+class ServerHttpUtils:
+
+    def __init__(self):
+        self.SERVER_HOST = '127.0.0.1'
+        self.SERVER_PORT = 8000
+        self.server_socket = None
+
+    def create_server(self):
+        # Create socket
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.server_socket.bind((self.SERVER_HOST, self.SERVER_PORT))
+        self.server_socket.listen(1)
+        print('Listening on port %s ...' % self.SERVER_PORT)
+        self.start_server()
+
+    def close_server(self):
+        # Close socket
+        print("Shutting down the server...")
+        self.server_socket.close()
+
+    def connect(self):
+        while True:
+            # Wait for client connections
+            client_connection, client_address = self.server_socket.accept()
+            # Get the client request
+            request = client_connection.recv(1024).decode()
+            print(request)
+            # Send HTTP response
+            response = 'HTTP/1.0 200 OK'
+            client_connection.sendall(response.encode())
+            client_connection.close()
+
+    def start_server(self):
+        # Set server connection for client as separate Thread
+        thread1 = Thread(target=self.connect)
+        thread1.start()
